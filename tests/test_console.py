21ea176f84e5c0346bc34bed07cb1fde9268149e
juanstm200 <juans.tovar70@gmail.com>
2021-11-05 21:40:38 -0500

    test console - Airbnb

---
+++ tests/test_console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Test"""
+
+import unittest
+from console import HBNBCommand
+
+
+class Test_Console(unittest.TestCase):
+    """ Unittest for console """
+
+if __name__ == '__main__':
+    unittest.main()
