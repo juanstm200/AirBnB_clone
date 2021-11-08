42b0d9978cac8c94d7aa130adbf51b8f52f585b0
juanstm200 <juans.tovar70@gmail.com>
2021-11-07 22:57:39 -0800

    _Console - Airbnb

--- console.py
+++ console.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains the entry point of the command interpreter
+ This module contains the entry point of the command interpreter
 """
 import cmd
 import sys
